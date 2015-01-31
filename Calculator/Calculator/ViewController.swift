7fb6cf37d952a81785d848f6a2bf3271bf1d76ce
Richard Ting <richard.tin@gmail.com>
2015-01-31 19:43:42 +0800

    Required Task#2: floating point number
    Add "." button for user to input floating point number

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -17,9 +17,15 @@ class ViewController: UIViewController
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTypingANumber {
-            display.text = display.text! + digit
+            if display.text!.rangeOfString(".") == nil || digit != "." {
+                display.text = display.text! + digit
+            }
         } else {
-            display.text = digit
+            if digit == "." {
+                display.text = display.text! + digit
+            } else {
+                display.text = digit
+            }
             userIsInTheMiddleOfTypingANumber = true
         }
     }
